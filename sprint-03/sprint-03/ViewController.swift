19b1d8890b1dfc95221036a1dd05a53cf245f98b
valentuk <pawapaw@yandex.ru>
2025-04-03 21:00:17 +0300

    Выполнил комментарии по ревью

--- sprint-03/sprint-03/ViewController.swift
+++ sprint-03/sprint-03/ViewController.swift
@@ -1,28 +1,24 @@
-//
-//  ViewController.swift
-//  sprint-03
-//
-//  Created by  Павел Валентюк on 02.04.2025.
-//
-
 import UIKit
 
 
 class ViewController: UIViewController {
+    var suranw: String = ""
+    private var count: Int = 0
     
-
-    //  Constant
-    var count: Int = 0
+    private func date() -> String {
+        var realdate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.YYYY" + "|" + "hh:mm:ss"
+        var datenow = dateFormatter.string(from: realdate)
+        return datenow
+    }
     
-    // Outlet
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var isCount: UILabel!
-    @IBOutlet weak var refreshCount: UIButton!
-    @IBOutlet weak var viewHistory: UITextView!
+    @IBOutlet weak private var plusButton: UIButton!
+    @IBOutlet weak private var minusButton: UIButton!
+    @IBOutlet weak private var isCount: UILabel!
+    @IBOutlet weak private var refreshCount: UIButton!
+    @IBOutlet weak private var viewHistory: UITextView!
     
-    
-    // Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
         viewHistory.isEditable = false
@@ -30,16 +26,14 @@ class ViewController: UIViewController {
         plusButton.tintColor = .red
         viewHistory.text = "История Изменений: \n"
     }
-
-    // Func actions
     
-    @IBAction func didPlusButton(_ sender: Any) {
+    @IBAction private func didPlusButton(_ sender: Any) {
         count += 1
         isCount.text = "\(count)"
         viewHistory.text += "\(date()): Значение изменено на +1\n"
     }
     
-    @IBAction func didMinusButton(_ sender: Any) {
+    @IBAction private func didMinusButton(_ sender: Any) {
         if count > 0 {
             count -= 1
             viewHistory.text += "\(date()): Значение изменено на -1\n"
@@ -50,19 +44,10 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func isRefreshCount(_ sender: Any) {
+    @IBAction private func isRefreshCount(_ sender: Any) {
         count = 0
         isCount.text = "\(count)"
         viewHistory.text += "\(date()): Значение сброшено\n"
     }
-    
-    private func date() -> String {
-        var realdate = Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd.MM.YYYY" + "|" + "hh:mm:ss"
-        var datenow = dateFormatter.string(from: realdate)
-        return datenow
-    }
-    
 }
 
